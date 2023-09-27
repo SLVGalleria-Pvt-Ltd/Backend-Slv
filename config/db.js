3228e2f388f7bc4927a4b90197f088dfc927f977
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-27 11:41:30 +0530

    ...

--- config/db.js
+++ config/db.js
@@ -1,24 +1,3 @@
-// import mongoose from "mongoose";
-// import colors from "colors";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const connectDB = async () => {
-//     try {
-//         const conn = await mongoose.connect(process.env.MONGODB_URL,
-//             {
-//                 useNewUrlParser: true,
-//                 useUnifiedTopology: true,
-//             });
-//         console.log(`Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white);
-//     } catch (error) {
-//         console.log(`Errro in Mongodb ${error}`.bgRed.white);
-//         process.exit(1);
-//     }
-// };
-
-// export default connectDB;
 
 
 const mongoose = require('mongoose');
--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(morgan("dev"));
 
 
 // routes mount 
-// app.use("/api/v1/auth", authRoutes);
-// app.use("/api/v1/category", categoryRoutes);
-// app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 
 
